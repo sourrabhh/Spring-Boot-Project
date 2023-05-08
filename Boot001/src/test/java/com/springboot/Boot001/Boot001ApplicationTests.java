06178e15116ab7a0344763e48401ebf4251cb36c
sourabh <souraabh@yahoo.com>
2023-05-08 18:59:26 +0530

    SPringboot Project

---
+++ Boot001/src/test/java/com/springboot/Boot001/Boot001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.Boot001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
