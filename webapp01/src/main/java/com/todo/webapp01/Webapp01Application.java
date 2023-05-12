fe4198af79be5da9bdadab0a8a43fa7f6a609813
sourabh <souraabh@yahoo.com>
2023-05-12 18:02:41 +0530

    .

---
+++ webapp01/src/main/java/com/todo/webapp01/Webapp01Application.java
@@ -0,0 +1,13 @@
+package com.todo.webapp01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webapp01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webapp01Application.class, args);
+	}
+
+}
---
+++ webapp01/src/test/java/com/todo/webapp01/Webapp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.webapp01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webapp01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
