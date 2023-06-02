00f9300284fba29f691a0c8873a78dd5b3dcfa4e
sourabh <souraabh@yahoo.com>
2023-06-02 18:23:25 +0530

    custome exception handler

---
+++ restful_web_services/src/main/java/com/restwebservice/restful_web_services/Exceptions/ErrorDetails.java
@@ -0,0 +1,32 @@
+
+package com.restwebservice.restful_web_services.Exceptions;
+import java.time.LocalDateTime;
+
+public class ErrorDetails 
+{
+    // Custome Exception Details
+    private LocalDateTime timestamp;
+    private String message;
+    private String details;
+
+    public ErrorDetails(LocalDateTime timestamp, String message, String details) 
+    {  
+        super();
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+  
+}
