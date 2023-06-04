0704f458c8344bc8d2a2a4099aff0584f7be4dbd
sourabh <souraabh@yahoo.com>
2023-06-04 12:59:38 +0530

    Delete user by ID

--- restful_web_services/src/main/java/com/restwebservice/restful_web_services/USER/UserDaoService.java
+++ restful_web_services/src/main/java/com/restwebservice/restful_web_services/USER/UserDaoService.java
@@ -37,4 +37,10 @@ public class UserDaoService
         users.add(userObj);
         return userObj;
     }
+
+    public void deleteById(int id)
+    {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        users.removeIf(predicate);
+    }
 }
