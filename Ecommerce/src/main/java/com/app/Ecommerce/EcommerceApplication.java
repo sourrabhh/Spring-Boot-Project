81d04c8dad7f16b712e7c603815cddef7b5cc0fc
sourabh <souraabh@yahoo.com>
2023-06-26 12:06:11 +0530

    initial commit

--- Ecommerce/src/main/java/com/app/Ecommerce/EcommerceApplication.java
+++ Ecommerce/src/main/java/com/app/Ecommerce/EcommerceApplication.java
@@ -1,4 +1,4 @@
-package com.app.Ecommerce;
+package com.app.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Ecommerce/src/test/java/com/app/Ecommerce/EcommerceApplicationTests.java
+++ Ecommerce/src/test/java/com/app/Ecommerce/EcommerceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.app.Ecommerce;
+package com.app.ecommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
